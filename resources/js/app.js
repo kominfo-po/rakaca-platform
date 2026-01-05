6f2a9c92b0ae8f24c43e1f4c3ea4f8e7e17b4ea4
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2026-01-05 14:06:58 +0700

    update js lib

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,26 @@ import sort from "@alpinejs/sort";
 // import toast
 import { showToast, registerToastListener } from "./toast";
 
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import Quote from "@editorjs/quote";
+import Code from "@editorjs/code";
+import Embed from "@editorjs/embed";
+import ImageTool from "@editorjs/image";
+import Sortable from "sortablejs";
+
 Alpine.plugin(slug);
 Alpine.plugin(sort);
+
+window.EditorJS = EditorJS;
+window.Header = Header;
+window.List = List;
+window.Quote = Quote;
+window.Code = Code;
+window.Embed = Embed;
+window.ImageTool = ImageTool;
+window.Sortable = Sortable;
 
 window.showToast = showToast;
 registerToastListener();
@@ -19,3 +37,4 @@ document.addEventListener("livewire:init", () => {
         });
     });
 });
+window.HSFileUpload;
